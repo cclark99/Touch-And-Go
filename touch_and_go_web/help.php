8ba6893e60240e18ca10a21b8cbc43cc62c54b6c
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-10-22 15:04:25 -0400

    update help.php
    Added </a> tags.

--- touch_and_go_web/help.php
+++ touch_and_go_web/help.php
@@ -1,56 +1,55 @@
-
-
 <!DOCTYPE html>
 
 <html lang="en"> <!-- start of html tag -->
-  <head> <!-- start of head tag -->
-    <!-- set charset -->
-    <meta charset="utf-8">
-    <!-- set title -->
-    <title>Help</title>
-    <!-- link external style.css sheet -->
-    <link rel = "stylesheet" type = "text/css" href = "../styles.css">
-    
-    <table> <!-- start of table tag -->
-      <thead> <!-- start of thead tag -->
-        <tr> <!-- start of row -->
-            
-          <th> <!-- start of Home cell -->
-            <h2><a class='link' href = "home.php">Home</h2>
-          </th> <!-- end of Home cell -->
-            
-          <th> <!-- start of Schedule cell -->
-            <h2><a class='link' href = "schedule.php">Schedule</h2>
-          </th> <!-- end of Schedule cell -->
-            
-          <th> <!-- start of Analytics cell -->
-            <h2><a class='link' href = "analytics.php">Analytics</h2>
-          </th> <!-- end of Analytics cell -->
-            
-          <th> <!-- start of Logo cell -->
-            <img src="../Touch__Go_Logo.jpg" alt="Touch and Go Logo" class="center" height="90">
-          </th><!-- end of Logo cell -->
-            
-          <th> <!-- start of Contact cell -->
-            <h2><a class='link' href = "contact.php">Contact</h2>
-          </th> <!-- end of Contact cell -->
-            
-          <th> <!-- start of Help cell -->
-            <h2><a class='link' href = "help.php">Help</h2>
-          </th> <!-- end of Help cell -->
-            
-          <th> <!-- start of Logout cell -->
-            <h2><a class='link' href = "logout.php">Logout</h2>
-          </th> <!-- end of Logout cell -->
-         
-        </tr> <!-- end of row -->
-      </thead> <!-- end of thead tag -->
-    </table> <!-- end of table tag -->
-    <!-- help header -->
-    <h1>Help</h1>
-  </head> <!-- end of head tag -->
-  
-  <body> <!-- start of body tag -->
-  </body> <!-- end of body tag -->
-  
+
+<head> <!-- start of head tag -->
+  <!-- set charset -->
+  <meta charset="utf-8">
+  <!-- set title -->
+  <title>Help</title>
+  <!-- link external style.css sheet -->
+  <link rel="stylesheet" type="text/css" href="../styles.css">
+
+  <table> <!-- start of table tag -->
+    <thead> <!-- start of thead tag -->
+      <tr> <!-- start of row -->
+
+        <th> <!-- start of Home cell -->
+          <h2><a class='link' href="home.php">Home</a></h2>
+        </th> <!-- end of Home cell -->
+
+        <th> <!-- start of Schedule cell -->
+          <h2><a class='link' href="schedule.php">Schedule</a></h2>
+        </th> <!-- end of Schedule cell -->
+
+        <th> <!-- start of Analytics cell -->
+          <h2><a class='link' href="analytics.php">Analytics</a></h2>
+        </th> <!-- end of Analytics cell -->
+
+        <th> <!-- start of Logo cell -->
+          <img src="../Touch__Go_Logo.jpg" alt="Touch and Go Logo" class="center" height="90">
+        </th><!-- end of Logo cell -->
+
+        <th> <!-- start of Contact cell -->
+          <h2><a class='link' href="contact.php">Contact</a></h2>
+        </th> <!-- end of Contact cell -->
+
+        <th> <!-- start of Help cell -->
+          <h2><a class='link' href="help.php">Help</a></h2>
+        </th> <!-- end of Help cell -->
+
+        <th> <!-- start of Logout cell -->
+          <h2><a class='link' href="logout.php">Logout</a></h2>
+        </th> <!-- end of Logout cell -->
+
+      </tr> <!-- end of row -->
+    </thead> <!-- end of thead tag -->
+  </table> <!-- end of table tag -->
+  <!-- help header -->
+  <h1>Help</h1>
+</head> <!-- end of head tag -->
+
+<body> <!-- start of body tag -->
+</body> <!-- end of body tag -->
+
 </html> <!-- end of html tag -->
