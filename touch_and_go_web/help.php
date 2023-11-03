123ff1e7154d1a3a4f0b126e1bd9a419279b4879
josephwebber <josephwebber18@gmail.com>
2023-11-03 13:42:39 -0400

    update help.php

--- touch_and_go_web/help.php
+++ touch_and_go_web/help.php
@@ -1,66 +1,55 @@
 <!DOCTYPE html>
 
 <html lang="en"> <!-- start of html tag -->
-
-<head> <!-- start of head tag -->
-  <!-- set charset -->
-  <meta charset="utf-8">
-  <!-- set title -->
-  <title>Help</title>
-  <!-- link external style.css sheet -->
-  <link rel="stylesheet" type="text/css" href="../styles.css">
-
-  <!-- start of style tag -->
-  <style>
+  <head> <!-- start of head tag -->
+    <!-- set charset -->
+    <meta charset="utf-8">
+    <!-- set title -->
+    <title>Help</title>
+    <!-- link external style.css sheet -->
+    <link rel = "stylesheet" type = "text/css" href = "../styles.css">
+    
+    <!-- start of style tag -->
+    <style>
       /* start of style rules for h3 tag */
       h3 {
         color: #10222E; /* make color blue */
         font-size: 24pt; /* make font size 24 pt */
         text-align: center; /* center align text */
-        margin-top: 5%; /* make margin-top 5% */
+        /*margin-top: 5%; /* make margin-top 5% */
        } /* end of style rules for h3 tag */
     </style> <!-- end of style tag -->
-
-  <table> <!-- start of table tag -->
-    <thead> <!-- start of thead tag -->
-      <tr> <!-- start of row -->
-
-        <th> <!-- start of Home cell -->
-          <h2><a class='link' href="home.php">Home</a></h2>
-        </th> <!-- end of Home cell -->
-
-        <th> <!-- start of Schedule cell -->
-          <h2><a class='link' href="schedule.php">Schedule</a></h2>
-        </th> <!-- end of Schedule cell -->
-
-        <th> <!-- start of Analytics cell -->
-          <h2><a class='link' href="analytics.php">Analytics</a></h2>
-        </th> <!-- end of Analytics cell -->
-
-        <th> <!-- start of Logo cell -->
-          <img src="../Touch__Go_Logo.jpg" alt="Touch and Go Logo" class="center" height="90">
-        </th><!-- end of Logo cell -->
-
-        <th> <!-- start of Contact cell -->
-          <h2><a class='link' href="contact.php">Contact</a></h2>
-        </th> <!-- end of Contact cell -->
-
-        <th> <!-- start of Help cell -->
-          <h2><a class='link' href="help.php">Help</a></h2>
-        </th> <!-- end of Help cell -->
-
-        <th> <!-- start of Logout cell -->
-          <h2><a class='link' href="logout.php">Logout</a></h2>
-        </th> <!-- end of Logout cell -->
-
-      </tr> <!-- end of row -->
-    </thead> <!-- end of thead tag -->
-  </table> <!-- end of table tag -->
-  <!-- help header -->
-  <h1>Help</h1>
-</head> <!-- end of head tag -->
+  </head> <!-- end of head tag -->
   
   <body> <!-- start of body tag -->
+    <!-- The following code was created on October 30, 2023, using 
+    information from the following link:
+    https://www.w3schools.com/css/css_navbar_horizontal.asp -->
+
+    <ul> <!-- start of ul for menu bar -->
+      <!-- list home.php link -->
+      <li><a class="link" href="home.php">Home</a></li>
+      <!-- list schedule.php link -->
+      <li><a class="link" href="schedule.php">Schedule</a></li>
+      <!-- list analytics.php link -->
+      <li><a class="link" href="analytics.php">Analytics</a></li>
+      <!-- list Touch & Go logo -->
+      <li><img src="../newLogo.png" alt="Touch and Go Logo" height="60"></li>
+      <!-- list contact.php link -->
+      <li><a class='link' href="contact.php">Contact</a></li>
+      <!-- list help.php link -->
+      <li><a class='link' href="help.php">Help</a></li>
+      <!-- list logout.php link -->
+      <li><a class='link' href="logout.php">Logout</a></li>
+    </ul> <!-- end of ul for menu bar -->
+   
+    <!-- this ends the code that was created using information from the 
+    following link:
+    https://www.w3schools.com/css/css_navbar_horizontal.asp -->
+
+    <!-- help header -->
+    <h1>Help</h1>
+  
     <!-- output "Issues with the site?"" -->
     <h2>Issues with the site?</h2>
     <!-- "Contact us at:"" -->
@@ -75,13 +64,13 @@
    https://www.youtube.com/watch?v=bwe-PsEoot4 -->
 
     <section class="dropdown-section"> <!-- start of section tag with dropdown-section class -->
-      <!-- FAQ header -->
-      <h3>Frequently Asked Questions</h3>
       <!-- horizintal line -->
       <hr>
+      <!-- FAQ header -->
+      <h3>Frequently Asked Questions</h3>
 
-      <ul class="dropdown"> <!-- start of ul tag with dropdown class -->
-        <li> <!-- start of li (first question) -->
+      <div class="dropdown"> <!-- start of ul tag with dropdown class -->
+       
           <div class="question"> <!-- start of div tag with question class -->
             <!-- create arrow -->
             <span class="arrow"></span>
@@ -92,9 +81,9 @@
             <!-- display answer to first question -->
             <p>We suggest contacting your professor first. If your professor has you marked as present on the specific day, please contact us via email or phone to resolve the problem.</p>
           </div> <!-- end of div tag -->
-        </li> <!-- end of li tag -->
+       
 
-        <li> <!-- start of li (second question) -->
+        
           <div class="question"> <!-- start of div tag with question class -->
             <!-- create arrow -->
             <span class="arrow"></span>
@@ -105,9 +94,9 @@
             <!-- display answer to second question -->
             <p>We will do our best to respond to all emails and phone calls within 2 business days.</p>
           </div> <!-- end of div tag -->
-        </li> <!-- end of li tag -->
+        
 
-        <li> <!-- start of li (third question) -->
+        
           <div class="question"> <!-- start of div tag with question class -->
             <!-- create arrow -->
             <span class="arrow"></span>
@@ -118,8 +107,8 @@
             <!-- display answer to third question -->
             <p>Each student's schedule will be posted one week before the start of the semester. If your schedule is not visible to you by this time, please reach out to us via email or phone.</p>
           </div> <!-- end of div tag -->
-        </li> <!-- end of li tag -->
-      </ul> <!-- end of ul tag -->
+        
+        </div> <!-- end of ul tag -->
     </section> <!-- end of section tag -->
 
     <!-- start of script tag -->
