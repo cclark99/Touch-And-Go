d45660900e19a37b307f6d61ee32c05f9ec2c954
Chris <cclar560@live.kutztown.edu>
2023-11-12 12:34:27 -0500

    scan update

--- Scan.py
+++ Scan.py
@@ -17,19 +17,19 @@ uart = serial.Serial("/dev/ttyUSB0", baudrate=57600, timeout=1)
 finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
 
 def get_fingerprint_name():
-    while True:
-        print("Waiting for a fingerprint scan...")
-        lcd.clear()
-        lcd.putstr("Place finger on sensor...")
+    print("Waiting for a fingerprint scan...")
+    lcd.clear()
+    lcd.putstr("Place finger on sensor...")
 
-        if finger.get_image() == adafruit_fingerprint.OK:
-            if finger.image_2_tz(1) == adafruit_fingerprint.OK:
-                if finger.finger_search() == adafruit_fingerprint.OK:
-                    lcd.clear()
-                    lcd.putstr("Fingerprint found")
-                    print("Fingerprint found")
-                    return finger.finger_id
-        time.sleep(1)  # Add a short delay to avoid continuous checking
+    while finger.get_image() != adafruit_fingerprint.OK:
+        pass
+    if finger.image_2_tz(1) == adafruit_fingerprint.OK:
+            if finger.finger_search() == adafruit_fingerprint.OK:
+                lcd.clear()
+                lcd.putstr("Fingerprint found")
+                print("Fingerprint found")
+                return finger.finger_id
+    time.sleep(1)  # Add a short delay to avoid continuous checking
 
 try:
     while True:
