260286c2beb5319d2ce47a149fcadba6009f7108
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-13 09:32:30 -0500

    Update analytics.php

--- touch_and_go_web/analytics.php
+++ touch_and_go_web/analytics.php
@@ -8,38 +8,7 @@ if (!isset($_SESSION['loggedin'])) {
 }
 
 require 'db_connection.php';
-
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if (
-  $stmt = $con->prepare('select course.courseId, 
-                                  courseName, 
-                                  courseDescription, 
-                                  courseDate, 
-                                  courseStartTime, 
-                                  courseEndTime, 
-                                  courseLocation 
-                            from course 
-                              inner join student_course on student_course.courseId = course.courseId
-                              inner join student on student.studentId = student_course.studentId 
-                            where student.studentId = ?')
-) {
-
-  $stmt->bind_param('s', $_SESSION['id']);
-
-  $stmt->execute();
-
-  $result = $stmt->get_result();
-
-  if ($result->num_rows > 0) {
-    $rows = $result->fetch_all(MYSQLI_ASSOC);
-    $course_array = array();
-    foreach ($rows as $row) {
-      $course_array[] = $row;
-    }
-  }
-  $stmt->close();
-  $con->close();
-}
+include 'get_course.php';
 
 ?>
 <!DOCTYPE html>
