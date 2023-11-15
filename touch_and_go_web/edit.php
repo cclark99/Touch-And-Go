72b20eabd1a9cf97da3b7069202a29778f7a0661
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-15 13:38:57 -0500

    Update edit.php

--- touch_and_go_web/edit.php
+++ touch_and_go_web/edit.php
@@ -98,7 +98,7 @@ if (!$userData) {
         <!-- <input type="text" name="firstName" value="...">
         <input type="text" name="lastName" value="..."> -->
 
-        <input type="number" name="userId" value="<?= htmlspecialchars($userData['userEmail']) ?>" hidden readonly>
+        <input type="number" name="userId" value="<?= htmlspecialchars($userData['userEmail']) ?>" readonly>
 
         <label for="userEmail">Email:</label>
         <input type="text" name="userEmail" value="<?= htmlspecialchars($userData['userEmail']) ?>"
