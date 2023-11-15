60ca965989d07f382645e977036ddfdf931b822f
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-15 13:34:47 -0500

    Update update_user.php

--- touch_and_go_web/update_user.php
+++ touch_and_go_web/update_user.php
@@ -22,43 +22,43 @@ $stmtUser->bind_param("si", $userEmail, $userId);
 $stmtUser->execute();
 
 // Check if the update was successful
-if ($stmtUser->affected_rows > 0) {
+// if ($stmtUser->affected_rows > 0) {
 
-    // Check the user type to update the corresponding table
-    switch ($userType) {
-        case 'student':
-            // Update the student table
-            $stmtStudent = $con->prepare("UPDATE student SET firstName = ?, lastName = ? WHERE userId = ?");
-            $stmtStudent->bind_param("ssi", $firstName, $lastName, $userId);
-            $stmtStudent->execute();
-            break;
+//     // Check the user type to update the corresponding table
+//     switch ($userType) {
+//         case 'student':
+//             // Update the student table
+//             $stmtStudent = $con->prepare("UPDATE student SET firstName = ?, lastName = ? WHERE userId = ?");
+//             $stmtStudent->bind_param("ssi", $firstName, $lastName, $userId);
+//             $stmtStudent->execute();
+//             break;
 
-        case 'professor':
-            // Update the professor table
-            $stmtProfessor = $con->prepare("UPDATE professor SET firstName = ?, lastName = ? WHERE userId = ?");
-            $stmtProfessor->bind_param("ssi", $firstName, $lastName, $userId);
-            $stmtProfessor->execute();
-            break;
+//         case 'professor':
+//             // Update the professor table
+//             $stmtProfessor = $con->prepare("UPDATE professor SET firstName = ?, lastName = ? WHERE userId = ?");
+//             $stmtProfessor->bind_param("ssi", $firstName, $lastName, $userId);
+//             $stmtProfessor->execute();
+//             break;
 
-        case 'admin':
-            // Update the admin table
-            $stmtAdmin = $con->prepare("UPDATE admin SET firstName = ?, lastName = ? WHERE userId = ?");
-            $stmtAdmin->bind_param("ssi", $firstName, $lastName, $userId);
-            $stmtAdmin->execute();
-            break;
+//         case 'admin':
+//             // Update the admin table
+//             $stmtAdmin = $con->prepare("UPDATE admin SET firstName = ?, lastName = ? WHERE userId = ?");
+//             $stmtAdmin->bind_param("ssi", $firstName, $lastName, $userId);
+//             $stmtAdmin->execute();
+//             break;
 
-        default:
-            // Handle unknown user types if needed
-            break;
-    }
+//         default:
+//             // Handle unknown user types if needed
+//             break;
+//     }
 
-    // Redirect back to the search page with a success message
-    header('Location: analytics.php?success=1');
-    exit();
-} else {
-    // Failed to update the user table (e.g., user not found)
-    // Redirect back to the search page with an error message
-    header('Location: analytics.php?error=1');
-    exit();
-}
+//     // Redirect back to the search page with a success message
+//     header('Location: analytics.php?success=1');
+//     exit();
+// } else {
+//     // Failed to update the user table (e.g., user not found)
+//     // Redirect back to the search page with an error message
+//     header('Location: analytics.php?error=1');
+//     exit();
+// }
 ?>
