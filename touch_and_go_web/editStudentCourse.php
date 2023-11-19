7efd467cc6cd5695ed8e7a0a8dc898796e23b2ed
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-19 10:44:08 -0500

    Update editStudentCourse.php

--- touch_and_go_web/editStudentCourse.php
+++ touch_and_go_web/editStudentCourse.php
@@ -79,6 +79,75 @@ $currentCoursesStmt->close();
     <title>Edit Student Courses</title>
     <link rel="stylesheet" type="text/css" href="../styles.css">
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a,
+        .dropbtn {
+            display: inline-block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover,
+        .dropdown:hover .dropbtn {
+            background-color: #555;
+        }
+
+        li.dropdown {
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        h2,
+        h3 {
+            color: #10222E;
+            text-align: center;
+            margin-top: 20px;
+        }
+
         form {
             max-width: 600px;
             margin: auto;
