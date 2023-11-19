722c1cd2f13158a1c43205bd2535eaca9ccb0338
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-19 13:19:28 -0500

    styling

--- touch_and_go_web/editProfessorCourse.php
+++ touch_and_go_web/editProfessorCourse.php
@@ -1,9 +1,8 @@
 <?php
-// editProfessorCourse.php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 session_start();
 
@@ -25,7 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($addCourseId) {
         // Add the course to the professor's courses
         $addCourseQuery = "INSERT INTO professor_course (userId, courseId) VALUES (?, ?)";
-        echo "Query: $addCourseQuery"; // Add this line for debugging
         $addCourseStmt = $con->prepare($addCourseQuery);
         $addCourseStmt->bind_param('ii', $professorId, $addCourseId);
         $addCourseStmt->execute();
@@ -35,7 +33,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($removeCourseId) {
         // Remove the course from the professor's courses
         $removeCourseQuery = "DELETE FROM professor_course WHERE userId = ? AND courseId = ?";
-        echo "Query: $removeCourseQuery"; // Add this line for debugging
         $removeCourseStmt = $con->prepare($removeCourseQuery);
         $removeCourseStmt->bind_param('ii', $professorId, $removeCourseId);
         $removeCourseStmt->execute();
@@ -44,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Redirect back to the page with a success message
     $_SESSION['updateMsg'] = 'Professor courses updated successfully';
-    header('Location: adminCourse.php');
+    header("Location: editProfessorCourse.php?professorId=$professorId&professorName=" . urlencode($professorName));
     exit();
 }
 
@@ -57,7 +54,6 @@ if (!is_numeric($professorId)) {
     header('Location: adminCourse.php');
     exit();
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -67,35 +63,142 @@ if (!is_numeric($professorId)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Professor Courses</title>
-    <!-- link external style.css sheet -->
     <link rel="stylesheet" type="text/css" href="../styles.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            position: fixed;
+            top: 0;
+            width: 100%;
+            z-index: 1000;
+        }
+
+        li {
+            float: left;
+            border-right: 1px solid #bbb;
+        }
+
+        li:last-child {
+            border-right: none;
+        }
+
+        .link {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        .link:hover {
+            background-color: #111;
+        }
+
+        #fakeNav {
+            display: block;
+            color: white;
+            padding: 14px 16px;
+        }
+
+        img {
+            margin-left: 20px;
+        }
+
+        h2 {
+            text-align: center;
+            margin-top: 70px;
+        }
+
+        form {
+            max-width: 600px;
+            margin: auto;
+            background-color: #FFFFFF;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
+        }
+
+        form label {
+            display: block;
+            margin-top: 10px;
+        }
+
+        form select {
+            width: 100%;
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            margin-top: 5px;
+            box-sizing: border-box;
+        }
+
+        form button {
+            width: 100%;
+            padding: 15px 0;
+            font-size: 18px;
+            border: 0;
+            color: #fff;
+            cursor: pointer;
+            border-radius: 5px;
+            background: #4CAF50;
+            transition: background 0.3s ease-in-out;
+            margin-top: 20px;
+        }
+
+        form button:hover {
+            background: #397d13;
+        }
+
+        div {
+            margin-bottom: 10px;
+        }
+
+        span {
+            color: black;
+            font-size: larger;
+        }
+
+        .no-courses-message {
+            text-align: center;
+            font-size: 18px;
+            margin-top: 20px;
+            color: #555;
+        }
+    </style>
 </head>
 
 <body>
 
-    <ul> <!-- start of ul for menu bar -->
-        <!-- list home.php link -->
+    <ul>
         <li><a class="link" href="adminHome.php">Home</a></li>
-        <!-- list schedule.php link -->
         <li><a class="link" href="adminCourse.php">Courses</a></li>
-        <!-- list analytics.php link -->
         <li id="fakeNav"><a></a></li>
-        <!-- list Touch & Go logo -->
         <li><img src="../newLogo.png" alt="Touch and Go Logo" height="60"></li>
-        <!-- list contact.php link -->
         <li id="fakeNav"><a></a></li>
-        <!-- list help.php link -->
         <li id="fakeNav"><a></a></li>
-        <!-- list logout.php link -->
         <li><a class='link' href="logout.php">Logout</a></li>
-    </ul> <!-- end of ul for menu bar -->
+    </ul>
 
-    <!-- Display current courses with delete buttons -->
     <h2>Current Courses for
         <?php echo $professorName; ?>
     </h2>
+
     <form method="post" action="editProfessorCourse.php">
         <input type="hidden" name="professorId" value="<?= $professorId ?>">
+        <input type="hidden" name="professorName" value="<?= $professorName ?>">
 
         <?php
         $currentCoursesQuery = "SELECT c.courseId, c.name FROM professor_course pc JOIN course c ON pc.courseId = c.courseId WHERE pc.userId = ?";
@@ -116,27 +219,29 @@ if (!is_numeric($professorId)) {
         ?>
     </form>
 
-    <!-- Add new courses form -->
     <h2>Add New Courses</h2>
+
     <form method="post" action="editProfessorCourse.php">
         <input type="hidden" name="professorId" value="<?= $professorId ?>">
+        <input type="hidden" name="professorName" value="<?= $professorName ?>">
 
-        <label for="addCourseId">Add Course:</label>
-        <!-- Assuming you have a table named 'course' with courseId and name columns -->
-        <select name="addCourseId">
-            <?php
-            $availableCoursesQuery = "SELECT courseId, name FROM course";
-            $availableCoursesResult = $con->query($availableCoursesQuery);
+        <div>
+            <label for="addCourseId">Add Course:</label>
+            <select name="addCourseId">
+                <?php
+                $availableCoursesQuery = "SELECT courseId, name FROM course";
+                $availableCoursesResult = $con->query($availableCoursesQuery);
 
-            while ($course = $availableCoursesResult->fetch_assoc()) {
-                echo "<option value='{$course['courseId']}'>{$course['name']}</option>";
-            }
+                while ($course = $availableCoursesResult->fetch_assoc()) {
+                    echo "<option value='{$course['courseId']}'>{$course['name']}</option>";
+                }
 
-            $availableCoursesResult->close();
-            ?>
-        </select>
+                $availableCoursesResult->close();
+                ?>
+            </select>
 
-        <button type="submit">Add Course</button>
+            <button type="submit">Add Course</button>
+        </div>
     </form>
 
 </body>
