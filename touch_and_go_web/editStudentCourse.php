802686e9052a67bf4e8ef44977b3d2dfe8c0ef1f
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-19 12:11:01 -0500

    Update editStudentCourse.php

--- touch_and_go_web/editStudentCourse.php
+++ touch_and_go_web/editStudentCourse.php
@@ -1,75 +1,3 @@
-<?php
-// editStudentCourse.php
-
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-
-session_start();
-
-if (!isset($_SESSION['loggedin']) || $_SESSION['userType'] != 'admin') {
-    include 'logout.php';
-    exit();
-}
-
-require 'db_connection.php';
-
-// Check if the form data is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Assuming you have form fields named 'studentId', 'addCourseId', and 'removeCourseId'
-    $studentId = $_POST['studentId'] ?? null;
-    $addCourseId = $_POST['addCourseId'] ?? null;
-    $removeCourseId = $_POST['removeCourseId'] ?? null;
-
-    if ($addCourseId) {
-        // Add the course to the student's courses
-        $addCourseQuery = "INSERT INTO student_course (userId, courseId) VALUES (?, ?)";
-        $addCourseStmt = $con->prepare($addCourseQuery);
-        $addCourseStmt->bind_param('ii', $studentId, $addCourseId);
-        $addCourseStmt->execute();
-        $addCourseStmt->close();
-    }
-
-    if ($removeCourseId) {
-        // Remove the course from the student's courses
-        $removeCourseQuery = "DELETE FROM student_course WHERE userId = ? AND courseId = ?";
-        $removeCourseStmt = $con->prepare($removeCourseQuery);
-        $removeCourseStmt->bind_param('ii', $studentId, $removeCourseId);
-        $removeCourseStmt->execute();
-        $removeCourseStmt->close();
-    }
-
-    // Redirect back to the page with a success message
-    $_SESSION['updateMsg'] = 'Student courses updated successfully';
-    header("Location: editStudentCourse.php?studentId=$studentId");
-    exit();
-}
-
-// Retrieve studentId from GET parameter
-$studentId = $_GET['studentId'] ?? null;
-$studentName = $_GET['studentName'] ?? null;
-
-// If studentId is not provided or not a valid number, redirect back
-if (!is_numeric($studentId)) {
-    header('Location: adminCourse.php');
-    exit();
-}
-
-// Fetch student's current courses
-$currentCoursesQuery = "SELECT c.courseId, c.name, c.prefix FROM student_course sc JOIN course c ON sc.courseId = c.courseId WHERE sc.userId = ?";
-$currentCoursesStmt = $con->prepare($currentCoursesQuery);
-$currentCoursesStmt->bind_param('i', $studentId);
-$currentCoursesStmt->execute();
-$currentCoursesStmt->bind_result($courseId, $courseName, $coursePrefix);
-$currentCourses = [];
-
-while ($currentCoursesStmt->fetch()) {
-    $currentCourses[] = ['courseId' => $courseId, 'courseName' => $courseName, 'coursePrefix' => $coursePrefix];
-}
-
-$currentCoursesStmt->close();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -136,11 +64,6 @@ $currentCoursesStmt->close();
 
         div {
             margin-bottom: 10px;
-            display: flex;
-            justify-content: space-between;
-            /* Align items horizontally with space between them */
-            align-items: center;
-            /* Align items vertically at the center */
         }
 
         span {
@@ -148,26 +71,36 @@ $currentCoursesStmt->close();
             font-size: larger;
         }
 
-        button {
-            padding: 10px;
-            font-size: 14px;
-            background-color: #e74c3c;
-            /* Use your preferred color */
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #c0392b;
-            /* Use your preferred hover color */
-        }
-
         h3 {
             text-align: center;
             font-size: xx-large;
             margin-top: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+
+        th,
+        td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        button {
+            padding: 5px;
+            font-size: 14px;
+            background-color: transparent;
+            /* No background color */
+            border: none;
+            cursor: pointer;
         }
     </style>
 </head>
@@ -191,15 +124,22 @@ $currentCoursesStmt->close();
     <form method="post" action="editStudentCourse.php">
         <input type="hidden" name="studentId" value="<?= $studentId ?>">
 
-        <?php
-        foreach ($currentCourses as $course) {
-            echo '<div>';
-            echo "<span>{$course['coursePrefix']} - </span>";
-            echo "<span>{$course['courseName']}</span>";
-            echo "<button type='submit' name='removeCourseId' value='{$course['courseId']}'>Remove</button>";
-            echo '</div>';
-        }
-        ?>
+        <table>
+            <tr>
+                <th>Prefix</th>
+                <th>Course Name</th>
+                <th>Edit</th>
+            </tr>
+            <?php
+            foreach ($currentCourses as $course) {
+                echo '<tr>';
+                echo "<td>{$course['coursePrefix']}</td>";
+                echo "<td>{$course['courseName']}</td>";
+                echo "<td><button type='submit' name='removeCourseId' value='{$course['courseId']}'>Edit</button></td>";
+                echo '</tr>';
+            }
+            ?>
+        </table>
     </form>
 
     <h3 class="center">Add New Courses</h3>
