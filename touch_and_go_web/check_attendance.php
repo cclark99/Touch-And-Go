8dbabbac5587a49578d6bc88e1317e173fc7a38c
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-22 12:56:23 -0500

    Update check_attendance.php

--- touch_and_go_web/check_attendance.php
+++ touch_and_go_web/check_attendance.php
@@ -3,57 +3,30 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+
 require 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $courseId = $_POST['courseId'] ?? null;
     $userId = $_SESSION['id'] ?? null;
 
-    // Get the current day and current week start and end dates
-    $currentDay = date('l'); // 'l' returns the full name of the day
-    $currentWeekStart = date('Y-m-d', strtotime('last Monday')); // Assuming Monday is the start of the week
-    $currentWeekEnd = date('Y-m-d', strtotime('next Sunday'));
-
-    // Check if the current day is within the daysOfWeek for the course and if the date is within the current week
-    $checkInQuery = "SELECT f.checkIn, c.startTime, c.endTime, c.startDate, c.endDate, c.daysOfWeek
-                 FROM fingerprint f
-                 JOIN course c ON f.checkIn BETWEEN CONCAT(c.startDate, ' ', c.startTime) AND CONCAT(c.endDate, ' ', c.endTime)
-                 WHERE f.userId = ? AND c.courseId = ? 
-                   AND (
-                       FIND_IN_SET(?, REPLACE(c.daysOfWeek, '', ',')) > 0
-                       OR c.daysOfWeek LIKE CONCAT('%', ?, '%') -- Check if the current day is in the daysOfWeek string
-                   )
-                   AND DAYOFWEEK(f.checkIn) = DAYOFWEEK(CURDATE())
-                   AND DATE(f.checkIn) BETWEEN ? AND ?
-                 ORDER BY f.checkIn DESC";
+    $checkInQuery = "SELECT f.checkIn, c.startTime, c.endTime, c.startDate, c.endDate
+                     FROM fingerprint f
+                     JOIN course c ON f.checkIn BETWEEN CONCAT(c.startDate, ' ', c.startTime) AND CONCAT(c.endDate, ' ', c.endTime)
+                     WHERE f.userId = ? AND c.courseId = ?
+                     ORDER BY f.checkIn desc";
 
     $checkInStmt = $con->prepare($checkInQuery);
-    $checkInStmt->bind_param('iissss', $userId, $courseId, $currentDay, $currentDay, $currentWeekStart, $currentWeekEnd);
+    $checkInStmt->bind_param('ii', $userId, $courseId);
     $checkInStmt->execute();
-    $checkInStmt->bind_result($checkIn, $startTime, $endTime, $startDate, $endDate, $daysOfWeek);
-
-    // Fetch all rows
-    $attendanceRecords = array();
-    while ($checkInStmt->fetch()) {
-        $attendanceRecords[] = array(
-            'checkIn' => $checkIn,
-            'startTime' => $startTime,
-            'endTime' => $endTime,
-            'startDate' => $startDate,
-            'endDate' => $endDate,
-            'daysOfWeek' => $daysOfWeek,
-        );
-    }
-
+    $checkInStmt->bind_result($checkIn, $startTime, $endTime, $startDate, $endDate);
+    $checkInStmt->fetch();
     $checkInStmt->close();
 
-    if (!empty($attendanceRecords)) {
-        // Output the attendance records for the current day and week
-        foreach ($attendanceRecords as $record) {
-            echo "You checked in at: {$record['checkIn']} during the class from {$record['startTime']} to {$record['endTime']} on {$record['startDate']}.";
-        }
+    if ($checkIn) {
+        echo "You checked in at: $checkIn during the class from $startTime to $endTime.";
     } else {
-        echo "No check-in records found for the specified class on $currentDay within the current week.";
+        echo "No check-in records found for the specified class.";
     }
 } else {
     // Handle the case where the request method is not POST
