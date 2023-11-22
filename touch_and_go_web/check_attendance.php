6ddcb5eed9f345d6639c484188755ff1e8fc2164
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-22 12:42:45 -0500

    Update check_attendance.php

--- touch_and_go_web/check_attendance.php
+++ touch_and_go_web/check_attendance.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                  ORDER BY f.checkIn DESC";
 
     $checkInStmt = $con->prepare($checkInQuery);
-    $checkInStmt->bind_param('iississs', $userId, $courseId, $currentDay, $currentDay, $currentDay, $currentDay, $currentWeekStart, $currentWeekEnd);
+    $checkInStmt->bind_param('iissss', $userId, $courseId, $currentDay, $currentDay, $currentWeekStart, $currentWeekEnd);
     $checkInStmt->execute();
     $checkInStmt->bind_result($checkIn, $startTime, $endTime, $startDate, $endDate, $daysOfWeek);
 
