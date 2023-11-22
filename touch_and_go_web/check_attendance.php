25a9007503c51de695d88813e997bf9c2c3b2106
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-22 12:58:31 -0500

    Check that checkIn is for currentDate

--- touch_and_go_web/check_attendance.php
+++ touch_and_go_web/check_attendance.php
@@ -3,30 +3,33 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 require 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $courseId = $_POST['courseId'] ?? null;
     $userId = $_SESSION['id'] ?? null;
 
+    // Get the current date
+    $currentDate = date('Y-m-d');
+
     $checkInQuery = "SELECT f.checkIn, c.startTime, c.endTime, c.startDate, c.endDate
                      FROM fingerprint f
                      JOIN course c ON f.checkIn BETWEEN CONCAT(c.startDate, ' ', c.startTime) AND CONCAT(c.endDate, ' ', c.endTime)
-                     WHERE f.userId = ? AND c.courseId = ?
-                     ORDER BY f.checkIn desc";
+                     WHERE f.userId = ? AND c.courseId = ? AND DATE(f.checkIn) = ?
+                     ORDER BY f.checkIn DESC
+                     LIMIT 1";  // Limit to one result since we only want the latest check-in
 
     $checkInStmt = $con->prepare($checkInQuery);
-    $checkInStmt->bind_param('ii', $userId, $courseId);
+    $checkInStmt->bind_param('iis', $userId, $courseId, $currentDate);
     $checkInStmt->execute();
     $checkInStmt->bind_result($checkIn, $startTime, $endTime, $startDate, $endDate);
     $checkInStmt->fetch();
     $checkInStmt->close();
 
     if ($checkIn) {
-        echo "You checked in at: $checkIn during the class from $startTime to $endTime.";
+        echo "You checked in at: $checkIn during the class from $startTime to $endTime on $startDate.";
     } else {
-        echo "No check-in records found for the specified class.";
+        echo "No check-in records found for the specified class on $currentDate.";
     }
 } else {
     // Handle the case where the request method is not POST
