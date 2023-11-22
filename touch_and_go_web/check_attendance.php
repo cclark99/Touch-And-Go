efea84612b4b34ce56c0bce17d91c271786f6869
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-22 10:38:05 -0500

    daysOfWeekCheck

--- touch_and_go_web/check_attendance.php
+++ touch_and_go_web/check_attendance.php
@@ -10,14 +10,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $courseId = $_POST['courseId'] ?? null;
     $userId = $_SESSION['id'] ?? null;
 
+    $currentDayOfWeek = date('l');
+
     $checkInQuery = "SELECT f.checkIn, c.startTime, c.endTime, c.startDate, c.endDate, c.daysOfWeek
                      FROM fingerprint f
                      JOIN course c ON f.checkIn BETWEEN CONCAT(c.startDate, ' ', c.startTime) AND CONCAT(c.endDate, ' ', c.endTime)
-                     WHERE f.userId = ? AND c.courseId = ?
+                     WHERE f.userId = ? AND c.courseId = ? AND LOCATE(?, c.daysOfWeek) > 0
                      ORDER BY f.checkIn desc";
 
     $checkInStmt = $con->prepare($checkInQuery);
-    $checkInStmt->bind_param('ii', $userId, $courseId);
+    $checkInStmt->bind_param('isi', $userId, $courseId, $currentDayOfWeek);
     $checkInStmt->execute();
     $checkInStmt->bind_result($checkIn, $startTime, $endTime, $startDate, $endDate, $daysOfWeek);
     $checkInStmt->fetch();
@@ -26,11 +28,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($checkIn) {
         echo "You checked in at: $checkIn during the class from $startTime to $endTime.";
     } else {
-        echo "No check-in records found for the specified class.";
+        echo "No check-in records found for the specified class on $currentDayOfWeek.";
     }
 } else {
     // Handle the case where the request method is not POST
     echo "Invalid request method.";
 }
+
 
 ?>
