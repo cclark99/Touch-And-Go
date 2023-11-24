784dbee594d0798663fee34fdebf862991bb37d6
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-24 12:50:41 -0500

    Update check_attendance.php

--- touch_and_go_web/check_attendance.php
+++ touch_and_go_web/check_attendance.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -9,48 +8,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $courseId = $_POST['courseId'] ?? null;
     $userId = $_SESSION['id'] ?? null;
 
-    // Get the current date
-    $currentDate = date('Y-m-d');
+    // Get the current date and time
+    $currentDateTime = date('Y-m-d H:i:s');
 
-    $checkInQuery = "SELECT f.checkIn, c.startTime, c.endTime, c.startDate, c.endDate
+    $checkInQuery = "SELECT f.checkIn
                      FROM fingerprint f
                      JOIN course c ON f.checkIn BETWEEN CONCAT(c.startDate, ' ', c.startTime) AND CONCAT(c.endDate, ' ', c.endTime)
                      WHERE f.userId = ? AND c.courseId = ? AND DATE(f.checkIn) = CURDATE()
                      ORDER BY f.checkIn DESC
-                     LIMIT 1";  // Limit to one result since we only want the latest check-in
+                     LIMIT 1";
 
     $checkInStmt = $con->prepare($checkInQuery);
 
     if (!$checkInStmt) {
-        // Debugging statement
         die("Prepare failed: (" . $con->errno . ") " . $con->error);
     } else {
         $checkInStmt->bind_param('ii', $userId, $courseId);
         $checkInStmt->execute();
 
         if ($checkInStmt->errno) {
-            // Debugging statement
             die("Execute failed: (" . $checkInStmt->errno . ") " . $checkInStmt->error);
         } else {
-            $checkInStmt->bind_result($checkIn, $startTime, $endTime, $startDate, $endDate);
-
-            // Debugging statement
-            echo "Debug SQL: " . $checkInQuery . "\n";
+            $checkInStmt->bind_result($checkIn);
 
             if ($checkInStmt->fetch()) {
-                // Debugging statement
-                echo "Fetched data successfully.\n";
-                echo "checkIn: $checkIn, startTime: $startTime, endTime: $endTime, startDate: $startDate, endDate: $endDate";
+                // Check if the retrieved check-in time is within an acceptable range
+                // You may need to adjust this condition based on your specific requirements
+                if (strtotime($checkIn) >= strtotime($currentDateTime) - 3600) {
+                    echo "Attendance recorded.";
+                } else {
+                    echo "No attendance recorded within the time range.";
+                }
             } else {
-                // Debugging statement
-                die("No data fetched. Error: " . $checkInStmt->error);
+                echo "No attendance recorded.";
             }
         }
 
         $checkInStmt->close();
     }
 } else {
-    // Handle the case where the request method is not POST
     echo "Invalid request method.";
 }
 ?>
