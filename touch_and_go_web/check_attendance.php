0a1a4c0a9bdadbfcd2fa0c08a32906d6961c13cb
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-24 13:43:01 -0500

    Update check_attendance.php

--- touch_and_go_web/check_attendance.php
+++ touch_and_go_web/check_attendance.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($checkInStmt->fetch()) {
                 // Format the attendance time in a readable format
                 $formattedTime = date('l, F j, Y g:i A', strtotime($checkIn));
-                echo "Attendance recorded. \nTime checked in: $formattedTime";
+                echo "Attendance recorded.</br>Time checked in: $formattedTime";
             } else {
                 echo "No attendance recorded.";
             }
