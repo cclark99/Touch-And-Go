aa477cf895206a867dea5a7276a8d2354f4a1cc8
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-26 14:27:47 -0500

    Update professorContact.php

--- touch_and_go_web/professorContact.php
+++ touch_and_go_web/professorContact.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['userType'] != 'professor') {
@@ -13,135 +12,113 @@ include 'get_professor_contact.php';
 ?>
 
 <!DOCTYPE html>
+<html lang="en">
 
-<html lang="en"> <!-- start of html tag -->
-
-<head> <!-- start of head tag -->
-  <!-- set charset -->
+<head>
   <meta charset="utf-8">
-  <!-- set title -->
   <title>Contact</title>
-  <!-- link external style.css sheet -->
   <link rel="stylesheet" type="text/css" href="../styles.css">
-
-  <!-- start of style tag -->
   <style>
-    /* start of style rules for h3 tag */
     h3 {
       color: #10222E;
-      /* make color blue */
       font-size: 24pt;
-      /* make font size 24 pt */
       text-align: center;
-      /* center align text */
-      /*margin-top: 5%; /* make margin-top 5% */
     }
 
-    /* end of style rules for h3 tag */
-
-    /* start of class style rules for dropdown */
-    .dropdown {
+    .dropdown-section {
       width: 25%;
-      /* make width 25% */
+      margin: auto;
     }
 
-    /* end of class style rules for dropdown */
-  </style> <!-- end of style tag -->
-</head> <!-- end of head tag -->
+    .dropdown {
+      list-style-type: none;
+      padding: 0;
+      cursor: pointer;
+    }
 
-<body> <!-- start of body tag -->
+    .question,
+    .answer {
+      display: none;
+    }
 
-  <!-- The following code was created on October 30, 2023, using 
-    information from the following link:
-    https://www.w3schools.com/css/css_navbar_horizontal.asp -->
+    .answer-opened {
+      display: block;
+    }
 
-  <ul> <!-- start of ul for menu bar -->
-    <!-- list home.php link -->
+    .arrow {
+      display: inline-block;
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 5px 5px 0;
+      border-color: #4CAF50 transparent transparent transparent;
+    }
+
+    .arrow-rotated {
+      transform: rotate(180deg);
+    }
+  </style>
+</head>
+
+<body>
+
+  <ul>
     <li><a class="link" href="professorHome.php">Home</a></li>
-    <!-- list schedule.php link -->
     <li><a class="link" href="professorSchedule.php">Schedule</a></li>
-    <!-- list analytics.php link -->
     <li><a class="link" href="professorAnalytics.php">Analytics</a></li>
-    <!-- list Touch & Go logo -->
     <li><img src="../newLogo.png" alt="Touch and Go Logo" height="60"></li>
-    <!-- list contact.php link -->
     <li><a class='link' href="professorContact.php">Contact</a></li>
-    <!-- list help.php link -->
     <li><a class='link' href="professorHelp.php">Help</a></li>
-    <!-- list logout.php link -->
     <li><a class='link' href="logout.php">Logout</a></li>
-  </ul> <!-- end of ul for menu bar -->
+  </ul>
 
-  <!-- this ends the code that was created using information from the 
-    following link:
-    https://www.w3schools.com/css/css_navbar_horizontal.asp -->
-
-  <!-- contact header -->
   <h1>Contact</h1>
-  <!-- display Professor Contact Information: -->
   <h3>Professor Contact Information:</h3>
 
-  <!-- The following code was created on October 16, 2023, using 
-    information from the following link:
-    https://www.youtube.com/watch?v=bwe-PsEoot4 */ -->
+  <section class="dropdown-section">
 
-  <section class="dropdown-section"> <!-- start of section tag with dropdown-section class -->
+    <?php
+    $printedCourseIds = array();
 
-    <ul class="dropdown"> <!-- start of ul tag with dropdown class -->
-      <?php
-      $printedCourseIds = array(); // Initialize an array to keep track of printed course IDs
-      
-      if ($course_array) {
-        foreach ($course_array as $row) {
-          $courseId = $row['courseId'];
+    if ($course_array) {
+      foreach ($course_array as $row) {
+        $courseId = $row['courseId'];
 
-          // Check if the course ID has already been printed
-          if (!in_array($courseId, $printedCourseIds)) {
-            echo '<div class="question"> <!-- start of div tag with question class -->
-                    <!-- create arrow -->
+        if (!in_array($courseId, $printedCourseIds)) {
+          echo '<ul class="dropdown"> <!-- start of ul tag with dropdown class -->
+                  <li class="question"> <!-- start of li tag with question class -->
                     <span class="arrow"></span>
-                    <!-- display course information -->
                     <span>' . $row['className'] . '</span>
-                  </div> <!-- end of div tag -->';
+                  </li> <!-- end of li tag -->';
 
-            // Add the course ID to the list of printed IDs
-            $printedCourseIds[] = $courseId;
-          }
-
-          // Display student information for the course
-          echo '<div class="answer"> <!-- start of div tag with answer class -->
-                <!-- display student information -->
-                <p>Student: ' . $row['firstName'] . ' ' . $row['lastName'] . '<br>
-                   Email: <a class="link" href="mailto:' . $row['userEmail'] . '">' . $row['userEmail'] . '</a>
-                </p>
-              </div><!-- end of div tag -->';
+          $printedCourseIds[] = $courseId;
         }
-      } else {
-        echo '<span style="color: #FAF8D6; line-height: 1.5em; padding-left: 2%; padding-right: 2%;">No student information found. </span>';
+
+        echo '<li class="answer"> <!-- start of li tag with answer class -->
+              <p>Student: ' . $row['firstName'] . ' ' . $row['lastName'] . '<br>
+                 Email: <a class="link" href="mailto:' . $row['userEmail'] . '">' . $row['userEmail'] . '</a>
+              </p>
+            </li><!-- end of li tag -->';
       }
-      ?>
-    </ul> <!-- end of ul tag -->
-  </section> <!-- end of section tag -->
 
-  <!-- start of script tag -->
+      echo '</ul>'; // close the ul tag
+    } else {
+      echo '<span style="color: #FAF8D6; line-height: 1.5em; padding-left: 2%; padding-right: 2%;">No student information found. </span>';
+    }
+    ?>
+  </section>
+
   <script>
-    // set variables
-    const question = document.querySelectorAll('.question');
-    const answer = document.querySelectorAll('.answer');
-    const arrow = document.querySelectorAll('.arrow');
+    const questions = document.querySelectorAll('.question');
 
-    // for loop to open the answer to the question
-    for (let i = 0; i < question.length; i++) {
-      question[i].addEventListener('click', () => {
-        answer[i].classList.toggle('answer-opened');
-        arrow[i].classList.toggle('arrow-rotated');
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].addEventListener('click', () => {
+        const answers = questions[i].nextElementSibling.querySelectorAll('.answer');
+        answers.forEach(answer => answer.classList.toggle('answer-opened'));
+        questions[i].querySelector('.arrow').classList.toggle('arrow-rotated');
       });
+    }
+  </script>
+</body>
 
-    } // end of for loop
-  </script> <!-- end of script tag -->
-  <!-- this ends the code that was created using information from the 
-    following link:
-    https://www.youtube.com/watch?v=bwe-PsEoot4 -->
-</body> <!-- end of body tag -->
-
-</html> <!-- end of html tag -->
+</html>
