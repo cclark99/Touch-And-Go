e96b3709a30e7bd4a74d545fdf7ac6c11921c6f0
Nathan Shutter <44408112+NathanShutter@users.noreply.github.com>
2023-11-30 15:37:54 -0500

    update message styling

--- touch_and_go_web/adminHome.php
+++ touch_and_go_web/adminHome.php
@@ -258,7 +258,7 @@ switch (true) {
             color: #fff;
             border-radius: 5px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: auto;
+            width: 50%;
         }
 
         .row-container button {
